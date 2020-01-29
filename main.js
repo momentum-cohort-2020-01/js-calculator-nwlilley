279e2857d322fe0cbddb4204597b10d0840bbb70
nwlilley <nathaniel.lilley@gmail.com>
2020-01-29 17:28:20 -0500

    HTML and CSS for calc

---
+++ main.js
@@ -0,0 +1,12 @@
+function buttonPress(buttonMessage) {
+    screen.innerHTML = <p>buttonMessage</p>;
+}
+
+
+
+// let clearButton = document.querySelector(".clear")
+// clearButton.addEventListener("click",  
+// })
+
+let sevenButton = document.querySelector(".seven")
+sevenButton.addEventListener("click", buttonPress("7"))
